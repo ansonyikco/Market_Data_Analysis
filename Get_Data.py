d708051cea98b2ffa5353e2088ff3b8d4ccb891a
ansonyikco <90606035+ansonyikco@users.noreply.github.com>
2025-01-10 17:00:44 +0800

    Update Get_Data.py
    Add different timeframe

--- Get_Data.py
+++ Get_Data.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import yfinance as yf
 
-def Get_Data(ticker_list):
+def Get_Data(ticker_list,mode):
 
     
     data = yf.download(ticker_list, interval='1mo',period='max')
@@ -12,6 +12,15 @@ def Get_Data(ticker_list):
     data['Date'] = data.index
     #mask = (data['Date'] > '2014-01-01')
     #data = data.loc[mask]
-    data = data[data['Date'].dt.month==1]
+    if mode == "Annual":
+        data = data[data['Date'].dt.month==1]
+    elif mode == "Semi-Annual":
+        data = data[data['Date'].dt.month.isin([1, 6])]
+    elif mode == "Quaterly":
+        data = data[data['Date'].dt.month.isin([1,3,6,9])]
+    elif mode == "Monthly":
+        pass
+        
+
     amount_of_asset = len(ticker_list)
     return data
